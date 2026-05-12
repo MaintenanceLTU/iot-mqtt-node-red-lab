59fdc27ce96b461c6d5b5d691f249c5bdd175284
Johan Odelius <johan.odelius@ltu.se>
2026-05-12 09:03:17 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -3,38 +3,39 @@ from datetime import datetime, UTC
 import time
 import ntplib
 
-class TimeClient:
+class NTPTimeClient:
     def __init__(self):
         self.ntp_offset = 0
         self.is_synced = False
             
-    def syncTime(self):    
+    def sync_ntp(self):    
         try:
-            c = ntplib.NTPClient()
-            response = c.request('europe.pool.ntp.org', version=3)
-            self.ntp_offset = response.tx_time - time.time()
+            client = ntplib.NTPClient()
+            response = client.request('europe.pool.ntp.org', version=3, timeout=5)
+            self.ntp_offset = response.offset
             self.is_synced = True
-            print("NTP synced")            
+            print(f"NTP synced. Offset: {self.ntp_offset:.3f} s")
         except Exception as e:
             self.is_synced = False
             print(f"NTP failed: {e}")
     
-    def getEpochTime(self, unit="ms"):    
-        epoch_time_s = time.time() + self.ntp_offset        
+    def get_epoch_time(self, unit="s"):            
+        t_s = time.time() + self.ntp_offset
         if unit=="s":
-            return epoch_time_s
+            return t_s
         elif unit=='ms':
-            return int(epoch_time_s*1e3)
+            return int(t_s*1e3)
         elif unit=='ns':
-            return int(epoch_time_s*1e9)        
+            # Warning: npt offset only valid for millisecond precision
+            # For nanosecond precision use time.time_ns()
+            return int(t_s*1e9)
         else:
-            print(f"unit {unit} not supported")
-            return None
+            raise ValueError(f"Invalid unit '{unit}'. Supported units are 's', 'ms', 'ns'.")            
     
-    def getIsoFormat(self):
-        t = self.getEpochTime(unit='s')
-        return datetime.fromtimestamp(t, UTC).isoformat()        
+    def get_isoformat(self):
+        t = self.get_epoch_time(unit='s')
+        return datetime.fromtimestamp(t, UTC).isoformat().replace('+00:00', 'Z')
     
-    def getFormattedDate(self, fmt='%Y%m%dT%H:%M:%S'):
-        t = self.getEpochTime(unit='s')
+    def get_formatted_date(self, fmt='%Y%m%dT%H:%M:%S'):
+        t = self.get_epoch_time(unit='s')
         return datetime.fromtimestamp(t, UTC).strftime(fmt)
